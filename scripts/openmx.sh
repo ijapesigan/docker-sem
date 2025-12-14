248197d8b7ae0605e91a5f4be0e60a9e8de40800
ijapesigan <ijapesigan@gmail.com>
2025-12-14 01:23:37 +0000

    Automated build.

--- scripts/openmx.sh
+++ scripts/openmx.sh
@@ -4,42 +4,52 @@ set -e
 
 # OpenMx dependencies
 install2.r --error --skipinstalled -n -1 \
-    BH           \
-    covr         \
-    digest       \
-    ggplot2      \
-    ifaTools     \
-    knitr        \
-    lifecycle    \
-    lme4         \
-    markdown     \
-    MASS         \
-    Matrix       \
-    mvtnorm      \
-    numDeriv     \
-    Rcpp         \
-    RcppEigen    \
-    RcppParallel \
-    reshape2     \
-    rmarkdown    \
-    roxygen2     \
-    rpf          \
-    snowfall     \
-    StanHeaders  \
+    BH            \
+    covr          \
+    digest        \
+    ggplot2       \
+    knitr         \
+    lifecycle     \
+    lme4          \
+    markdown      \
+    MASS          \
+    Matrix        \
+    mvtnorm       \
+    numDeriv      \
+    Rcpp          \
+    RcppArmadillo \
+    RcppEigen     \
+    RcppGSL       \
+    RcppTOML      \
+    RcppParallel  \
+    reshape2      \
+    rmarkdown     \
+    roxygen2      \
+    rpf           \
+    snowfall      \
+    StanHeaders   \
     testthat
 
-R -e "                                                 \
-  install.packages(                                    \
-    'OpenMx',                                          \
-    contriburl = contrib.url(                          \
-      repos = 'https://openmx.ssri.psu.edu/software/', \
-      type = 'source'                                  \
-    ),                                                 \
-    dependencies = NA,                                 \
-    verbose = TRUE                                     \
-  );                                                   \
-  demo('BivariateCorrelation', package = 'OpenMx')     \
-"
+# OpenMx
+install2.r --error --skipinstalled -n -1 \
+    OpenMx
+
+# R -e "                                                 \
+#   install.packages(                                    \
+#     'OpenMx',                                          \
+#     contriburl = contrib.url(                          \
+#       repos = 'https://openmx.ssri.psu.edu/software/', \
+#       type = 'source'                                  \
+#     ),                                                 \
+#     dependencies = NA,                                 \
+#     verbose = TRUE                                     \
+#   );                                                   \
+#   demo('BivariateCorrelation', package = 'OpenMx')     \
+# "
+
+# Item factor analysis in OpenMx
+install2.r --error --skipinstalled -n -1 \
+    ifaTools
 
 R -e "library(OpenMx)"
 
